234e3918caffa5a946f62f526625d871ce466f73
riccardo tinto <tinto.riccardo@iisgalvanimi.edu.it>
2024-03-15 10:20:01 +0000

    d

---
+++ rubrica/src/app/lista-contatti/lista-contatti.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-contatti',
+  standalone: true,
+  imports: [],
+  templateUrl: './lista-contatti.component.html',
+  styleUrl: './lista-contatti.component.css'
+})
+export class ListaContattiComponent {
+
+}
